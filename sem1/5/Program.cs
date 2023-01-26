2c56d456173cde711ad3f7467b9fb421a80e66dd
AntonH <anton160687@gmail.com>
2023-01-26 22:02:25 +0300

    задача 7

---
+++ sem1/5/Program.cs
@@ -0,0 +1,6 @@
+﻿//На входе трехзначное на выходе последняя цифра
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+number = number % 10;
+ Console.WriteLine($"{number}");
