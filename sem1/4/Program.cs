90a14e2411a88f278b97334aa712067856dfc9a9
AntonH <anton160687@gmail.com>
2023-01-26 23:14:06 +0300

    seminar

---
+++ sem1/4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int i = -1 * number;
+while(i <= number){
+    Console.Write($"{i},");
+    i++; // i = i + 1
+}
