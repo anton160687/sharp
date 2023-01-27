2c7505c862f63f35981d332c91ee9db425ded9eb
AntonH <anton160687@gmail.com>
2023-01-27 10:49:35 +0300

    задача 4

---
+++ sem1/domz/task4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число с:");
+int c = int.Parse(Console.ReadLine());
+int max;
+if(a>b && a>c) {
+ max = a; 
+} else if (b>a && b>c){
+ max = b;
+} else {
+    max = c;
+}
+Console.WriteLine($"Максимальное число {max}");
