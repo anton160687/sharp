a0b618f9c0a43bd99ce4c6f64ff44afc8e9c964d
AntonH <anton160687@gmail.com>
2023-01-27 11:32:21 +0300

    задача 8

--- sem1/domz/task8/Program.cs
+++ sem1/domz/task8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+int i=1;
+while(i <= a){
+   if(i % 2==0){
+    Console.Write($"{i},");
+    }
+    i++; // i = i + 1
+}
