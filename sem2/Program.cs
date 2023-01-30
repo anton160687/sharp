b99f99e3c5539a7b75a9260ca6cc31b22b8c6859
AntonH <anton160687@gmail.com>
2023-01-30 21:00:31 +0300

    задача sim2

--- sem2/Program.cs
+++ sem2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4  -> кратно
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+int result = a % b;
+if (result == 0) {
+    Console.Write($"Кратно ");
+} else {
+    Console.Write($"Остаток {result} ");
+}
