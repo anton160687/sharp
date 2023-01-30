4f8802dbb2590cf7db04ab3a82728cabcbb08779
AntonH <anton160687@gmail.com>
2023-01-30 22:15:39 +0300

    задача sim2-3

--- sem2-3/Program.cs
+++ sem2-3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+if (a*a == b || b*b == a) {
+    Console.Write("да");
+} else {
+    Console.Write("нет");
+}
