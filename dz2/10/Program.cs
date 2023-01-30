5d73cb4e865eda95bf221f1e23da317bb11dfbb2
AntonH <anton160687@gmail.com>
2023-01-30 23:46:11 +0300

    задача dz2 task10

--- dz2/10/Program.cs
+++ dz2/10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+a = a / 10 % 10;
+Console.Write($"Введите трехзначное число: {a}");
