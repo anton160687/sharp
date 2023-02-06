6e1a22a3a1347c37cac25f0d7107a71b6eb8b911
AntonH <anton160687@gmail.com>
2023-02-06 20:20:10 +0300

    дз3

--- dz3/19/Program.cs
+++ dz3/19/Program.cs
@@ -3,34 +3,22 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
-// Console.Clear();
-// Console.WriteLine("Введите пятизначное число:");
-// string num = Console.ReadLine();
-// if (num.Length == 5)
-// {
-//     if((num[0] == num[4])&(num[1] == num[3]))
-//     {
-//         Console.WriteLine($"{num} -> да");
-//     }
-//     else
-//     {
-//         Console.WriteLine($"{num} -> нет");
-//     }
-// }
 Console.Clear();
- Console.WriteLine("Введите пятизначное число:");
-int colection = int.Parse(Console.ReadLine());
-int[] arr = {colection};
-void ReversArray(int[] arr) {
-    int left;    
-    int right;    
-    int length = arr.Length;
-    right= length/2;
-    for(int i = 0; right == length-1; i<right;) {
-        int j = arr[i];
-
-        j = arr[i-k];
-    }
-
-   
+Console.Write("Введите число : ");
+int num = int.Parse(Console.ReadLine());int Revers(int num){
+    int revers = 0;// 54321
+    while (num > 0)
+        { // num = 0 > 0                        54320
+            revers = revers * 10 + num % 10; // revers = 5432 * 10 + num%10(1%10 = 1) = 54321
+            num /= 10; // num / 10(1/10)
+        }// 12345 == 54321
+    return revers;
+}
+if (num == Revers(num))
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
 }
--- dz3/23/Program.cs
+++ dz3/23/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+for(int i = 1; num >= i; i++) {
+    int[] arr = {i};
+    Console.WriteLine($"{i*i*i}");
+}
+
