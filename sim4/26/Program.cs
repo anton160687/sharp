e124e7b19955c6a5390a729bce5fcc81fd0dd31c
AntonH <anton160687@gmail.com>
2023-02-06 20:58:27 +0300

    задача 28 семинар 4

--- sim4/26/Program.cs
+++ sim4/26/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//24 Напишите программу, которая принимает на вход число A
+// и выдаёт сумму чисел от 1 до A
+// a = 5
+// sum = 1 + 2 + 3 + 4 + 5
+// 26 Напишите программу,
+//  которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int GetNum(int number){
+    int count = 0;
+    if(number<0) {
+       number *= (-1);
+    }
+    while(number > 0){
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine(GetNum(number));
