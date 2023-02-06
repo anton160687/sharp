4cc25ed85642d9b441c78b43218a613af1e293ce
AntonH <anton160687@gmail.com>
2023-02-06 12:43:32 +0300

    sem3

--- dz3/19/Program.cs
+++ dz3/19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.WriteLine("Введите пятизначное число:");
+string num = Console.ReadLine()!;
+if (num.Length == 5)
+{
+    if((num[0] == num[4])&(num[1] == num[3]))
+    {
+        Console.WriteLine($"{num} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{num} -> нет");
+    }
+}
